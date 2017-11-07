6efed814e8878a10699b9077d74939559bf3a020
Kike <marquezoj@karposviajes.com>
2017-11-07 11:25:47 -0400

    Creating a matrix N*N*N from instruction set given.
    @isCreatingNexMatrix -- instruction : String -> bool
    @createMatrix -- n : Number -> Object(dimension:value)
    @addNumberToTheEnd -- accumulatedArray : Array , number: Number -> Array(String)
    @getAllNumbersAsStringArray -- upperLimit : Number -> Array(String)

--- cube-problem.js
+++ cube-problem.js
@@ -2,7 +2,39 @@
  * Created by Kike on 11/7/17.
  */
 function processData(input) {
+    var instructions = input.split("\n");
+    let testCases;
+    var matrix = {};
+    var matrixDimension, operations;
+    instructions.map((instruction) -> {
+        if(instruction.length == 1){
+            testCases = parseInt(instruction);
+        } else if(isCreatingNewMatrix(instruction)){
+            [matrixDimension, operations] = instruction.split(' ');
+            matrix = createMatrix(parseInt(matrixDimension))
+        } else {
 
+        }
+    })
+}
+
+function isCreatingNewMatrix(instruction){
+    return instruction.split(' ').length == 2 && !isNaN(parseInt(instruction.split(' ')[0])) && !isNaN(parseInt(instruction.split(' ')[1]));
+}
+
+function createMatrix(n){
+    //get All Numbers -> add all numbers to each number (1,1 ; 1,2; 1,3; 2,1 ,etc) -> remove helper at the beggining -> add all numbers again
+    return getAllNumbersAsStringArray(n).reduce(addNumberToTheEnd,[n]).slice(1).reduce(addNumberToTheEnd,[n]).slice(1).reduce((matrixObject,key) -> {
+        matrixObject[key] = 0;
+        return matrixObject
+    },{});
+}
+
+function addNumberToTheEnd(accumulatedArray,number) {
+    return accumulatedArray.concat(getAllNumbersAsStringArray(accumulatedArray[0]).map(numberAsString -> number + "." + numberAsString));
+}
+function getAllNumbersAsStringArray(upperLimit){
+    return [...Array(upperLimit)].map((_,i) -> i+1 ).map(e -> e.toString())
 }
 
 process.stdin.resume();
@@ -15,3 +47,5 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
     processData(_input);
 });
+
+
